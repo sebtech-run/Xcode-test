a113ec8d2ddab6fbd5209e1a01620aedfbdcc386
sebtech974 <sebtech974@gmail.com>
2024-08-10 17:09:31 +0400

    Initial Commit

---
+++ TutoXcode1/TutoXcode1App.swift
@@ -0,0 +1,17 @@
+//
+//  TutoXcode1App.swift
+//  TutoXcode1
+//
+//  Created by SEBASTIEN BOUILLET on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TutoXcode1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
